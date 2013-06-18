bf62f43674b464a40ac89c3b67ec39347c78b62e
seringeek <serinpaul@outlook.com>
2013-06-18 11:50:49 +0530

    Playing with zurbs foundation framework

---
+++ foundation_test/app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :name, :created_at, :updated_at
---
+++ foundation_test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoundationTest::Application.config.secret_key_base = '26f2c51504b66f9c1f7647359f42b62b5b23339306e9ffa6a58ecf80166e282c2376e0959d986ac23b2b3faa7d4c13526afc584905c547186bc9cf781370ece6'
