b742f4858c459ecd9c0c5fda20449ce9c51d7277
seringeek <serinpaul@outlook.com>
2013-06-18 11:50:06 +0530

    playing with twitter bootstrap

---
+++ test_twtr_bootstrap/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestTwtrBootstrap::Application.config.secret_key_base = '6a178805481d80144751098bdb9a1f2126325f139676808205fb714f2eb6038630f17476eff9c0aef27d8f94d8f08463daed076f5a22f9fd2fc0b3bbd9df65f2'
